9dd22edce736e31c49df3c8491f31cdfd413ca83
halzathalik <halzathalik@gmail.com>
2023-08-28 23:34:42 -0400

    initial commit

---
+++ Spring-01-Core-IoC/src/main/java/org/example/bean_annotation/CydeoApp.java
@@ -0,0 +1,15 @@
+package org.example.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
+        FullTimeMentor ft = container.getBean(FullTimeMentor.class);
+        ft.createAccount();
+        String str = container.getBean(String.class);
+        System.out.println(str);
+    }
+}
