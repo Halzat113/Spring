116ccae996dfd838d3bcf56e7d081c90dfe1eb88
halzathalik <halzathalik@gmail.com>
2023-09-05 16:51:38 -0400

    springboot starter

---
+++ Spring-06-SpringBoot-Demo/src/main/java/org/example/CydeoApplication.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.model.Comment;
+import org.example.service.CommentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class CydeoApplication {
+
+    public static void main(String[] args) {
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = SpringApplication.run(CydeoApplication.class, args);
+
+        CommentService cs = context.getBean(CommentService.class);
+        cs.publishComment(comment);
+    }
+
+}
